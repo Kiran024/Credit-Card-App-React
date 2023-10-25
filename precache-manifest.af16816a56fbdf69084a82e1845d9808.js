d533c145f27039c8d61eb1ff2a017df7bd124ac2
Kiran024 <145450942+Kiran024@users.noreply.github.com>
2023-10-25 16:51:06 +0530

    Updates

---
+++ precache-manifest.af16816a56fbdf69084a82e1845d9808.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a78ebb4ef691aa21223fc1c10d17b68",
+    "url": "/Credit-Card-App-React/index.html"
+  },
+  {
+    "revision": "a9df2572bbb85d8925ab",
+    "url": "/Credit-Card-App-React/static/css/main.840aef6f.chunk.css"
+  },
+  {
+    "revision": "b60167b7c42cb3382aeb",
+    "url": "/Credit-Card-App-React/static/js/2.52dd3d26.chunk.js"
+  },
+  {
+    "revision": "c563299317d52c1696cd",
+    "url": "/Credit-Card-App-React/static/js/3.937f6cc8.chunk.js"
+  },
+  {
+    "revision": "a9df2572bbb85d8925ab",
+    "url": "/Credit-Card-App-React/static/js/main.e90879e1.chunk.js"
+  },
+  {
+    "revision": "b4f06ca7af72497afb61",
+    "url": "/Credit-Card-App-React/static/js/runtime~main.fc264dc4.js"
+  },
+  {
+    "revision": "7f5f2c71d4c06e4afb31c70af281f92e",
+    "url": "/Credit-Card-App-React/static/media/color.7f5f2c71.png"
+  },
+  {
+    "revision": "ca4ac67c3ef52ea31c0c940d8cd3bc5b",
+    "url": "/Credit-Card-App-React/static/media/front.ca4ac67c.png"
+  }
+]);
